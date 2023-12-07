32a0be6c2f0c6146673c94487b010b68a46ca5c6
Alex Spanos <10924953+alejio@users.noreply.github.com>
2023-12-07 21:29:17 +0000

    passed tests

--- aoc_day_4.py
+++ aoc_day_4.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from math import floor
+
 import regex as re
 
 
@@ -6,6 +8,12 @@ import regex as re
 class Scratchcard:
     winning_numbers: list[int]
     drawn_numbers: list[int]
+
+    def count_set_of_drawn_numbers_that_were_winning(self) -> int:
+        return len(set(self.winning_numbers).intersection(set(self.drawn_numbers)))
+
+    def calculate_points_per_game(self) -> int:
+        return floor(2**(self.count_set_of_drawn_numbers_that_were_winning()-1))
 
 
 def load_and_parse_txt_lines(filename: str) -> list[str]:
@@ -17,3 +25,11 @@ def load_and_parse_txt_lines(filename: str) -> list[str]:
         list_of_scratchcards = [Scratchcard(list(map(int, line[0].split())),
                                             list(map(int, line[1].split()))) for line in list_of_lines]
         return list_of_scratchcards
+
+
+def main_day_4(input_file: str) -> int:
+    list_of_scratchcards = load_and_parse_txt_lines(input_file)
+    total_count = 0
+    for scratchcard in list_of_scratchcards:
+        total_count += scratchcard.calculate_points_per_game()
+    return total_count
--- tests/test_aoc_day_4.py
+++ tests/test_aoc_day_4.py
@@ -1,7 +1,16 @@
-from aoc_day_4 import load_and_parse_txt_lines, Scratchcard
+from aoc_day_4 import load_and_parse_txt_lines, Scratchcard, main_day_4
 
 
 def test_load_and_parse_txt_lines():
     expected = Scratchcard([41, 48, 83, 86, 17], [83, 86, 6, 31, 17, 9, 48, 53])
     full_output = load_and_parse_txt_lines("fixtures/test_fixture_day_4.txt")
     assert full_output[0] == expected
+
+
+def test_count_set_of_drawn_numbers_that_were_winning():
+    scratchcard = Scratchcard([41, 48, 83, 86, 17], [83, 86, 6, 31, 17, 9, 48, 53])
+    assert scratchcard.count_set_of_drawn_numbers_that_were_winning() == 4
+
+
+def test_main_day_4():
+    assert main_day_4("fixtures/test_fixture_day_4.txt") == 13
